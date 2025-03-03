1cf918e5a17f1fcbb95fc41ffe6ed32fb09a39c2
zymioyz <zymioyz@gmail.com>
2025-03-04 10:25:38 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -102,9 +102,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var temp = activeVideo;
     activeVideo = inactiveVideo;
     inactiveVideo = temp;
-    setTimeout(function() {
-      activeVideo.addEventListener("click", handleClick);
-    }, 50);
   }
 
   // 点击事件处理：
